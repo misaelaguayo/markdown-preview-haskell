12eb1cdbad69ce881c05d54ac5e0dfa1be8294f6
misael aguayo <aguayo.misael@gmail.com>
2023-09-23 19:20:10 -0500

    Convert markdown to sixel and save in sixel file

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-# markdown-preview-haskell
+markdown-preview-haskell
 
-## Setup
+Setup
 
 - `stack build` to compile program
 - `stack build --file-watch --exec markdown-preview-haskell-exe` to build and hot reload changes
 - `stack ghci` to load and debug single functions
+
+How mark down is converted
+
+- Scan markdown from file
+- Convert scanned markdown into latex string
+- Output latex string into temporary file
+- Run `pdflatex` program with latex file input to convert to pdf
+- Run ghostcript (`gs`) to convert pdf to png
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,45 @@
 module Lib
-    ( someFunc
+    ( someFunc,
+      latexStr,
+      latexToSixel,
+      prettify,
+      headerSize
     ) where
+
+import System.Directory (getCurrentDirectory)
+import System.Process (readProcessWithExitCode, readCreateProcessWithExitCode, shell)
+import Data.List.Split (splitOn)
+
+latexStr :: String -> String
+latexStr str = 
+    "\\documentclass{article}\n"
+    ++ "\\begin{document}\n"
+    ++ str
+    ++ "\\end{document}\n"
+
+latexToSixel :: String -> IO ()
+latexToSixel str = do
+    dir <- getCurrentDirectory
+    let sixelDir = dir ++ "/sixel"
+    writeFile (sixelDir ++ "/sixel.tex") (latexStr str)
+    (_, _, _) <- readProcessWithExitCode "pdflatex" ["-output-directory=" ++ sixelDir, sixelDir ++ "/sixel.tex"] ""
+    (_, _, _) <- readProcessWithExitCode "gs" ["-dSAFER", "-r75", "-sDEVICE=png16m", "-o", sixelDir ++ "/sixel.png", sixelDir ++ "/sixel.pdf"] ""
+    let createSixelCommand = "convert " ++ sixelDir ++ "/sixel.png ppm:- | ppmquant 16 | ppmtosixel -margin > " ++ sixelDir ++ "/sixel.sixel"
+    readCreateProcessWithExitCode (shell createSixelCommand) ""
+    >>= \(_, _, _) -> return ()
+
+prettify :: String -> (Float, String)
+prettify contents
+    | headerSize splitSentence > 0 = (headerSize splitSentence, unwords (drop 1 splitSentence))
+    | otherwise = (0, contents)
+    where splitSentence = splitOn " " contents
+
+headerSize :: [String] -> Float
+headerSize splitSentence
+    | head splitSentence == "#" = 4
+    | head splitSentence == "##" = 2.5
+    | head splitSentence == "##" = 2
+    | otherwise = 0
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
